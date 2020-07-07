e0246b7d17da1a2c4c23727a3578e43f54887742
Matanel Abayof <MatanelAbayof@outlook.com>
2020-07-07 21:52:22 +0300

    sync

--- code/main.py
+++ code/main.py
@@ -1,10 +1,8 @@
 from keras.preprocessing.image import ImageDataGenerator
-import os
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
 
 print("Hello DL project!")
 
@@ -15,3 +13,44 @@ dataset = datagen.flow_from_directory(dataset_dir, class_mode='binary', batch_si
 
 num_of_writers = 407
 y_true = np.arange(num_of_writers)
+
+n_train_samples = 133
+n_validation_samples = 133
+n_test_samples = num_of_writers - n_validation_samples - n_train_samples
+epochs = 10
+batch_size = 5
+
+img_width = 5000
+img_height = 7000
+input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=n_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=n_validation_samples // batch_size)
